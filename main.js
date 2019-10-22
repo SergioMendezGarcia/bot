6ca339838402cfbc59794d84e5f3465990b6930d
Sergio Mendez Garcia <sergio.mendezgarcia.0702@gmail.com>
2019-10-22 02:58:46 +0200

    Nuevos comandos redes horario hora, actualizacion de roll, y timeout por enlace y mayúsculas excesivas

--- main.js
+++ main.js
@@ -24,13 +24,59 @@ client.on('connected', (address, port) => {
 });
 
 client.on('chat', (channel, user, message, self) => {
-    if (message === '!test') {
-        client.action('chechubot', 'Esto es un comando de prueba');
+    if (self) return;
+    let d = new Date();
+
+    let viewer = user['display-name'];
+
+    // if (message === '!dive') {
+    //     dive++;
+    //     if(dive < 2){
+    //         client.action(`${channel}`, `Dalvenger se ha estampado ${dive} vez este stream.`);
+    //     }else{
+    //         client.action(`${channel}`, `Dalvenger se ha estampado ${dive} veces este stream.`);
+    //     }
+    // }
+
+    if (message === '!redes') {
+        client.action(channel, 'Sigueme en mis redes sociales para no perderte novedades sobre el stream. Twitter: https://cutt.ly/UegGywg Instagram: https://cutt.ly/aegGy3h');
+    }
+
+    if (message === '!horario') {
+        client.action(channel, 'Hago stream de lunes a jueves de 8 de la tarde a 12 de la noche, y sábados de 6 de la tarde a 12+ Hora peninsular española. Si eres de latam y quieres comprobar que hora es aquí, utiliza el comando !hora');
+    }
+
+    if (message === '!hora') {
+        if (d.getHours() <= 12) {
+            client.action(channel, `@${viewer} en españa son las ${d.getHours()}:${d.getMinutes()} AM`);
+        }else {
+            client.action(channel, `@${viewer} en españa son las ${d.getHours()}:${d.getMinutes()}`);
+        }
     }
 
     if (message === '!roll') {
-        var dado = Math.floor(Math.random()* 10);
+        var dado = Math.floor(Math.random()* 6)+1;
         //`@${user['display-name']}`
-    client.action('chechubot', `@${user['display-name']} te ha salido un ${dado}`);
+        if ( dado < 3 ) {
+            client.action(`${channel}`, `@${viewer} te ha salido un ${dado}, suerte la próxima KEKW`);
+        }
+        if ( dado >= 3 ) {
+            client.action(`${channel}`, `@${viewer} te ha salido un ${dado}, Pog `);
+        }
     }
+
+    if (user['mod'] === false) {
+        if(message.includes('www.' || message.includes('.com') || message.includes('.net') || message.includes('.es'))){
+            client.timeout(channel, viewer, 300, 'Se ha detectado un enlace no permitido');
+            client.action(channel, `@${viewer} No está permitido poner enlaces en el chat, un moderador debe revisarlo primero, puedes ver que moderadores hay con /mod`);
+        }
+    }
+
+    if (user['mod'] === false) {
+        if(message.includes('COME') || message.includes('BEBE AGUA') || message.includes('BEBE') || message.includes('AGUA')){
+            client.timeout(channel, viewer, 5, 'Too much hydrated');
+            client.action(channel, `@${viewer} Estar bien hidratado y alimentado es bien, pero espamear en mayúsculas no`);
+        }
+    }
+
 });
